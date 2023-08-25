e9ad2749201abd0d54bcef8dd2fce5b6730906ba
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-25 20:54:02 +0800

    ccc

--- fourpaws-child/includes/shortcodes.php
+++ fourpaws-child/includes/shortcodes.php
@@ -15,6 +15,46 @@ function slider($atts, $content = null)
     ob_start();
 ?>
     <div class="hero-slider">
+        <!-- Swiper -->
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <?php foreach ($slides as $slide) { ?>
+                    <?php
+                    $image = $slide['image'];
+                    $heading = $slide['heading'];
+                    $description = $slide['description'];
+                    $button_text_1 = $slide['button_text_1'];
+                    $button_link_1 = $slide['button_link_1'];
+                    $button_text_2 = $slide['button_text_2'];
+                    $button_link_2 = $slide['button_link_2'];
+                    ?>
+                    <div class="swiper-slide">
+                        <div class="eltdf-row-grid-section">
+                            <div class="heading-box">
+                                <h2><?= $heading ?></h2>
+                            </div>
+                            <div class="description-box">
+                                <?= wpautop($description) ?>
+                            </div>
+                            <div class="button-group-box">
+                                <?php if ($button_text_1) { ?>
+                                    <a itemprop="url" href="<?= $button_link_1 ?>" target="_self" style="color: #ffffff;margin: 0 15px 0 0" class="eltdf-btn eltdf-btn-medium eltdf-btn-solid eltdf-btn-arrow">
+                                        <span class="eltdf-btn-text"><?= $button_text_1 ?></span>
+                                    </a>
+                                <?php } ?>
+                                <?php if ($button_text_2) { ?>
+                                    <a itemprop="url" href="<?= $button_link_2 ?>" target="_self" style="color: #ffffff" class="eltdf-btn eltdf-btn-medium eltdf-btn-outline eltdf-btn-arrow">
+                                        <span class="eltdf-btn-text"><?= $button_text_2 ?></span>
+                                    </a>
+                                <?php } ?>
+
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
 
     </div>
 <?php
