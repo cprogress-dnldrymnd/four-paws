c1842ee8b00515e1876075889131f18f0cff3912
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-25 20:46:08 +0800

    cxc

--- fourpaws-child/functions.php
+++ fourpaws-child/functions.php
@@ -2,14 +2,15 @@
 
 /*** Child Theme Function  ***/
 
-if ( ! function_exists( 'academist_elated_child_theme_enqueue_scripts' ) ) {
-	function academist_elated_child_theme_enqueue_scripts() {
+if (!function_exists('academist_elated_child_theme_enqueue_scripts')) {
+	function academist_elated_child_theme_enqueue_scripts()
+	{
 		$parent_style = 'academist-elated-default-style';
-		
-		wp_enqueue_style( 'academist-elated-child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
+
+		wp_enqueue_style('academist-elated-child-style', get_stylesheet_directory_uri() . '/style.css', array($parent_style));
 	}
-	
-	add_action( 'wp_enqueue_scripts', 'academist_elated_child_theme_enqueue_scripts' );
+
+	add_action('wp_enqueue_scripts', 'academist_elated_child_theme_enqueue_scripts');
 }
 /*-----------------------------------------------------------------------------------*/
 /* Register Carbofields
@@ -21,3 +22,38 @@ function tissue_paper_register_custom_fields()
 }
 require_once('includes/post-types.php');
 require_once('includes/shortcodes.php');
+
+function get__post_meta($value)
+{
+	if (function_exists('carbon_get_the_post_meta')) {
+		return carbon_get_the_post_meta($value);
+	} else {
+		return 'Error: Carbonfield not activated';
+	}
+}
+
+function get__term_meta($term_id, $value)
+{
+	if (function_exists('carbon_get_term_meta')) {
+		return carbon_get_term_meta($term_id, $value);
+	} else {
+		return 'Error: Carbonfield not activated';
+	}
+}
+
+function get__post_meta_by_id($id, $value)
+{
+	if (function_exists('carbon_get_post_meta')) {
+		return carbon_get_post_meta($id, $value);
+	} else {
+		return 'Error: Carbonfield not activated';
+	}
+}
+function get__theme_option($value)
+{
+	if (function_exists('carbon_get_theme_option')) {
+		return carbon_get_theme_option($value);
+	} else {
+		return 'Error: Carbonfield not activated';
+	}
+}
--- fourpaws-child/includes/shortcodes.php
+++ fourpaws-child/includes/shortcodes.php
@@ -1,7 +1,18 @@
 <?php
 
-function slider()
+function slider($atts, $content = null)
 {
+    extract(
+        shortcode_atts(
+            array(
+                'id' => '',
+            ),
+            $atts
+        )
+    );
+
+    $slides = get__post_meta_by_id($id, 'slides');
+    var_dump($slides);
 }
 
 add_shortcode('slider', 'slider');
