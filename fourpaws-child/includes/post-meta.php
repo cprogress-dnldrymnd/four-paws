7a535018399118f3562145742510a99f18d87ecc
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-25 20:24:01 +0800

    Update post-meta.php

--- fourpaws-child/includes/post-meta.php
+++ fourpaws-child/includes/post-meta.php
@@ -8,22 +8,3 @@ use Carbon_Fields\Field;
 /*-----------------------------------------------------------------------------------*/
 /* Webinar
 /*-----------------------------------------------------------------------------------*/
-
-Container::make('post_meta', 'Webinar Settings')
-	->set_priority('high')
-	->or_where('post_type', '=', 'slider')
-	->add_fields(
-		array(
-			Field::make('complex', 'Slider', __('Slider'))
-				->add_fields(array(
-					Field::make('text', 'image', __('Background Image')),
-					Field::make('text', 'alt_title', __('Alt Title')),
-					Field::make('textarea', 'description', __('Description')),
-					Field::make('text', 'button_text_1', __('Button Text[1'))->set_width(50),
-					Field::make('text', 'button_link_1', __('Button Link[1'))->set_width(50),
-					Field::make('text', 'button_text_2', __('Button Text[1'))->set_width(50),
-					Field::make('text', 'button_link_2', __('Button Link[2'))->set_width(50),
-
-				))
-		)
-	);
