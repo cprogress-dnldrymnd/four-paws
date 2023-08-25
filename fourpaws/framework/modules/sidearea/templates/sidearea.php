ceb30fb06da4095a8973e17bd1097d1d3c678145
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-25 21:48:19 +0800

    dsds

--- fourpaws/framework/modules/sidearea/templates/sidearea.php
+++ fourpaws/framework/modules/sidearea/templates/sidearea.php
@@ -1,12 +1,8 @@
-<?php if (!get__theme_option('hide_sidearea')) { ?>
-
-	<section class="eltdf-side-menu">
-		<a <?php academist_elated_class_attribute($close_icon_classes); ?> href="#">
-			<?php echo academist_elated_get_icon_sources_html('side_area', true); ?>
-		</a>
-		<?php if (is_active_sidebar('sidearea')) {
-			dynamic_sidebar('sidearea');
-		} ?>
-	</section>
-
-<?php } ?>
+<section class="eltdf-side-menu">
+	<a <?php academist_elated_class_attribute( $close_icon_classes ); ?> href="#">
+		<?php echo academist_elated_get_icon_sources_html( 'side_area', true ); ?>
+	</a>
+	<?php if ( is_active_sidebar( 'sidearea' ) ) {
+		dynamic_sidebar( 'sidearea' );
+	} ?>
+</section>
