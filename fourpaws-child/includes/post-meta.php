f92509325c39c6301eb5ccbd43b9376eff13483c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-25 22:37:37 +0800

    Update post-meta.php

--- fourpaws-child/includes/post-meta.php
+++ fourpaws-child/includes/post-meta.php
@@ -7,7 +7,16 @@ use Carbon_Fields\Field;
 Container::make('theme_options', __('Theme Option'))
 	->add_fields(
 		array(
-			Field::make('checkbox', 'hide_sidearea', __('Hide SideArea')),
+			Field::make('complex', 'our_schools', 'Our Schools')
+				->add_fields(
+					array(
+						Field::make('text', 'school', __('School')),
+
+					)
+				)
+				->set_layout('tabbed-vertical')
+				->set_header_template('<%- school  %>'),
+
 
 		)
 	);
