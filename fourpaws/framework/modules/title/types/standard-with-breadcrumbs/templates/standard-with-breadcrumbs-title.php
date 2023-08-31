9fa861ef8a255c993f827fff6e9523e40a6ba7e2
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-08-31 13:46:52 +0800

    Update standard-with-breadcrumbs-title.php

--- fourpaws/framework/modules/title/types/standard-with-breadcrumbs/templates/standard-with-breadcrumbs-title.php
+++ fourpaws/framework/modules/title/types/standard-with-breadcrumbs/templates/standard-with-breadcrumbs-title.php
@@ -1,7 +1,16 @@
 <?php do_action('academist_elated_action_before_page_title'); ?>
-<?= $title ?>
+<?php 
+if (is_archive()) { 
+	$title = get_the_archive_title();
+} else {
+	$title = $title;
+}
+?>
+
+
+
 <div class="eltdf-title-holder <?php echo esc_attr($holder_classes); ?>" <?php academist_elated_inline_style($holder_styles); ?> <?php echo academist_elated_get_inline_attrs($holder_data); ?>>
-	<?php if(!empty($title_image)) { ?>
+	<?php if (!empty($title_image)) { ?>
 		<div class="eltdf-title-image">
 			<img itemprop="image" src="<?php echo esc_url($title_image['src']); ?>" alt="<?php echo esc_attr($title_image['alt']); ?>" />
 		</div>
@@ -10,10 +19,10 @@
 		<div class="eltdf-title-inner">
 			<div class="eltdf-grid">
 				<div class="eltdf-title-info">
-					<?php if(!empty($title)) { ?>
+					<?php if (!empty($title)) { ?>
 						<<?php echo esc_attr($title_tag); ?> class="eltdf-page-title entry-title" <?php academist_elated_inline_style($title_styles); ?>><?php echo esc_html($title); ?></<?php echo esc_attr($title_tag); ?>>
 					<?php } ?>
-					<?php if(!empty($subtitle)){ ?>
+					<?php if (!empty($subtitle)) { ?>
 						<<?php echo esc_attr($subtitle_tag); ?> class="eltdf-page-subtitle" <?php academist_elated_inline_style($subtitle_styles); ?>><?php echo esc_html($subtitle); ?></<?php echo esc_attr($subtitle_tag); ?>>
 					<?php } ?>
 					<div class="eltdf-breadcrumbs-info">
@@ -21,7 +30,7 @@
 					</div>
 				</div>
 			</div>
-	    </div>
+		</div>
 	</div>
 </div>
 
