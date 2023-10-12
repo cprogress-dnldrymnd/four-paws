513dd06f6b89299863f104b45ee6468f4b70c5fa
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-12 20:36:09 +0800

    Update excerpt.php

--- fourpaws-child/framework/modules/blog/templates/parts/excerpt.php
+++ fourpaws-child/framework/modules/blog/templates/parts/excerpt.php
@@ -1,18 +1,26 @@
 <?php
 
-if ( post_password_required() ) {
+if (post_password_required()) {
 	echo get_the_password_form();
 } else {
-	$excerpt_length = isset( $params['excerpt_length'] ) ? $params['excerpt_length'] : '';
-	$excerpt        = academist_elated_excerpt( $excerpt_length );
-	
-	$link_page_exists = apply_filters( 'academist_elated_filter_single_links_exists', '' );
-	
-	if ( ! empty( $excerpt ) && empty( $link_page_exists ) ) { ?>
+	$excerpt_length = isset($params['excerpt_length']) ? $params['excerpt_length'] : '';
+	$excerpt        = academist_elated_excerpt($excerpt_length);
+
+	$link_page_exists = apply_filters('academist_elated_filter_single_links_exists', '');
+
+	if (!empty($excerpt) && empty($link_page_exists)) { ?>
 		<div class="eltdf-post-excerpt-holder">
 			<p itemprop="description" class="eltdf-post-excerpt">
-				<?php echo wp_kses_post( $excerpt ); ?>
+				<?php echo wp_kses_post($excerpt); ?>
 			</p>
 		</div>
 	<?php }
+
+	?>
+	<div class="button-box">
+		<a itemprop="url" href="<?= get_the_permalink() ?>" target="_self" class="eltdf-btn eltdf-btn-medium eltdf-btn-solid eltdf-btn-arrow">
+			<span class="eltdf-btn-text">Our Courses</span>
+		</a>
+	</div>
+<?php
 } ?>
