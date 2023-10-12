d44dac0a416b517ca2b6a58ddbfe330dae34a23f
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-12 21:49:14 +0800

    c

--- fourpaws-child/framework/modules/blog/templates/singles/standard/post.php
+++ fourpaws-child/framework/modules/blog/templates/singles/standard/post.php
@@ -1,13 +1,17 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <div class="eltdf-post-content">
-        <div class="eltdf-post-heading">
-            <?php academist_elated_get_module_template_part('templates/parts/media', 'blog', $post_format, $part_params); ?>
+        <div class="top-info">
+            <div class="row">
+                <div class="col-auto">
+                    <?= post_category('single-page'); ?>
+                </div>
+            </div>
         </div>
         <div class="eltdf-post-text">
             <div class="eltdf-post-text-inner">
                 <div class="eltdf-post-info-top">
                     <?php academist_elated_get_module_template_part('templates/parts/post-info/category', 'blog', '', $part_params); ?>
-					<?php academist_elated_get_module_template_part('templates/parts/post-info/date', 'blog', '', $part_params); ?>
+                    <?php academist_elated_get_module_template_part('templates/parts/post-info/date', 'blog', '', $part_params); ?>
                 </div>
                 <div class="eltdf-post-text-main">
                     <?php academist_elated_get_module_template_part('templates/parts/title', 'blog', '', $part_params); ?>
@@ -16,10 +20,10 @@
                 </div>
                 <div class="eltdf-post-info-bottom clearfix">
                     <div class="eltdf-post-info-bottom-left">
-						<?php academist_elated_get_module_template_part('templates/parts/post-info/tags', 'blog', '', $part_params); ?>
+                        <?php academist_elated_get_module_template_part('templates/parts/post-info/tags', 'blog', '', $part_params); ?>
                     </div>
                     <div class="eltdf-post-info-bottom-right">
-						<?php academist_elated_get_module_template_part('templates/parts/post-info/share', 'blog', '', $part_params); ?>
+                        <?php academist_elated_get_module_template_part('templates/parts/post-info/share', 'blog', '', $part_params); ?>
                     </div>
                 </div>
             </div>
