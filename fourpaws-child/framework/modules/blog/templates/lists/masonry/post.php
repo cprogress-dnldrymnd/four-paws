bb0edd06773aac4945876b13da7bb64ccf54d077
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-12 20:42:08 +0800

    cc

--- fourpaws-child/framework/modules/blog/templates/lists/masonry/post.php
+++ fourpaws-child/framework/modules/blog/templates/lists/masonry/post.php
@@ -8,17 +8,19 @@ $post_classes[] = 'eltdf-item-space';
         </div>
         <div class="eltdf-post-text">
             <div class="eltdf-post-text-inner">
-                <div class="eltdf-post-info-top">
-                    <?php academist_elated_get_module_template_part('templates/parts/post-info/category', 'blog', '', $part_params); ?>
-					<?php academist_elated_get_module_template_part('templates/parts/post-info/date', 'blog', '', $part_params); ?>
-                </div>
                 <div class="eltdf-post-text-main">
                     <?php academist_elated_get_module_template_part('templates/parts/title', 'blog', '', $part_params); ?>
+					<?php academist_elated_get_module_template_part('templates/parts/post-info/date', 'blog', '', $part_params); ?>
                     <?php academist_elated_get_module_template_part('templates/parts/excerpt', 'blog', '', $part_params); ?>
+                    <div class="button-box">
+                        <a itemprop="url" href="<?= get_the_permalink() ?>" target="_self" class="eltdf-btn eltdf-btn-medium eltdf-btn-solid eltdf-btn-arrow">
+                            <span class="eltdf-btn-text">Read more</span>
+                        </a>
+                    </div>
                 </div>
                 <div class="eltdf-post-info-bottom clearfix">
                     <div class="eltdf-post-info-bottom-left">
-						<?php academist_elated_get_module_template_part('templates/parts/post-info/tags', 'blog', '', $part_params); ?>
+                        <?php academist_elated_get_module_template_part('templates/parts/post-info/tags', 'blog', '', $part_params); ?>
                     </div>
                     <div class="eltdf-post-info-bottom-right">
                         <?php academist_elated_get_module_template_part('templates/parts/post-info/share', 'blog', '', $part_params); ?>
--- fourpaws-child/framework/modules/blog/templates/parts/post-info/category.php
+++ fourpaws-child/framework/modules/blog/templates/parts/post-info/category.php
@@ -1,5 +1,3 @@
-<!--
 <div class="eltdf-post-info-category">
-    <?php //the_category(', '); ?>
+    <?php the_category(', '); ?>
 </div>
--->
