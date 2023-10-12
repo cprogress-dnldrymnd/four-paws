309de101c616d32c3c6110d53cd9464436cad11c
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-12 17:29:41 +0800

    Update courses.php

--- fourpaws-child/includes/courses.php
+++ fourpaws-child/includes/courses.php
@@ -185,9 +185,9 @@ function course_details()
                         <svg xmlns="http://www.w3.org/2000/svg" width="15.008" height="20.034" viewBox="0 0 15.008 20.034">
                             <path id="award-svgrepo-com_2_" data-name="award-svgrepo-com (2)" d="M14.2,15.344a5.5,5.5,0,0,0,1.977-.884l1.147,4.507a1.675,1.675,0,0,1-2.392,1.859L13,19.9a2.32,2.32,0,0,0-2,0l-1.933.928a1.675,1.675,0,0,1-2.392-1.859L7.82,14.46a5.5,5.5,0,0,0,1.977.884m4.4,0a9.95,9.95,0,0,1-4.4,0m4.4,0a5.342,5.342,0,0,0,4.043-3.915,9.052,9.052,0,0,0,0-4.266A5.342,5.342,0,0,0,14.2,3.247a9.95,9.95,0,0,0-4.4,0A5.342,5.342,0,0,0,5.755,7.162a9.055,9.055,0,0,0,0,4.266A5.342,5.342,0,0,0,9.8,15.344" transform="translate(-4.496 -1.996)" fill="none" stroke="#7f3e98" stroke-width="2" />
                         </svg>
+                        Location(s):
                     </span>
                     <span class="text"><?= $locations ?></span>
-                    Location(s):
                 </div>
             <?php } ?>
             <?php if ($price) { ?>
@@ -198,7 +198,7 @@ function course_details()
                     </div>
                 </div>
             <?php } ?>
-            </div>
+        </div>
     </div>
 
 <?php
