e8a6a4440bf1f58b2e0909b94b59d51fbb085c11
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-12 21:50:54 +0800

    ccc

--- fourpaws-child/framework/modules/blog/templates/singles/standard/post.php
+++ fourpaws-child/framework/modules/blog/templates/singles/standard/post.php
@@ -5,14 +5,14 @@
                 <div class="col-auto">
                     <?= post_category('single-page'); ?>
                 </div>
+                <div class="col-auto">
+                    <?php academist_elated_get_module_template_part('templates/parts/post-info/date', 'blog', '', $part_params); ?>
+                </div>
             </div>
         </div>
         <div class="eltdf-post-text">
             <div class="eltdf-post-text-inner">
-                <div class="eltdf-post-info-top">
-                    <?php academist_elated_get_module_template_part('templates/parts/post-info/category', 'blog', '', $part_params); ?>
-                    <?php academist_elated_get_module_template_part('templates/parts/post-info/date', 'blog', '', $part_params); ?>
-                </div>
+            
                 <div class="eltdf-post-text-main">
                     <?php academist_elated_get_module_template_part('templates/parts/title', 'blog', '', $part_params); ?>
                     <?php the_content(); ?>
