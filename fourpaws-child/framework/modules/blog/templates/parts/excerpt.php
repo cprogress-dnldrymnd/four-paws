f6cbfa568054ffff535f24f3143197ad553e14d7
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-12 20:34:07 +0800

    Update excerpt.php

--- fourpaws-child/framework/modules/blog/templates/parts/excerpt.php
+++ fourpaws-child/framework/modules/blog/templates/parts/excerpt.php
@@ -1,33 +1,18 @@
 <?php
 
-if (post_password_required()) {
+if ( post_password_required() ) {
 	echo get_the_password_form();
 } else {
-	$excerpt_length = isset($params['excerpt_length']) ? $params['excerpt_length'] : '';
-	$excerpt        = academist_elated_excerpt($excerpt_length);
-
-	$link_page_exists = apply_filters('academist_elated_filter_single_links_exists', '');
-
-	if (!empty($excerpt) && empty($link_page_exists)) { ?>
+	$excerpt_length = isset( $params['excerpt_length'] ) ? $params['excerpt_length'] : '';
+	$excerpt        = academist_elated_excerpt( $excerpt_length );
+	
+	$link_page_exists = apply_filters( 'academist_elated_filter_single_links_exists', '' );
+	
+	if ( ! empty( $excerpt ) && empty( $link_page_exists ) ) { ?>
 		<div class="eltdf-post-excerpt-holder">
 			<p itemprop="description" class="eltdf-post-excerpt">
-				<?php echo wp_kses_post($excerpt); ?>
+				<?php echo wp_kses_post( $excerpt ); ?>
 			</p>
 		</div>
 	<?php }
-
-	?>
-	<div class="eltdf-post-read-more-button">
-		<?php
-		$button_params = array(
-			'type'         => 'simple',
-			'link'         => get_the_permalink(),
-			'text'         => esc_html__('Read More', 'academist'),
-			'custom_class' => 'eltdf-blog-list-button'
-		);
-
-		echo academist_elated_return_button_html($button_params);
-		?>
-	</div>
-<?php
 } ?>
