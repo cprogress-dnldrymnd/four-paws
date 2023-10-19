29ee0aba9daa61c2a7bb57ac4e8bd29fce1e6d8c
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-10-19 19:27:38 +0800

    Update post-types.php

--- fourpaws-child/includes/post-types.php
+++ fourpaws-child/includes/post-types.php
@@ -171,7 +171,7 @@ function action_register_post_type_args_instructor($args, $post_type)
 		return $args;
 	}
 	$rewrite = array(
-		'slug'                  => 'location',
+		'slug'                  => 'locations',
 		'with_front'            => true,
 		'pages'                 => true,
 		'feeds'                 => true,
