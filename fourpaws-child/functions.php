e3003767e4c9a26df5c7e7ac852d54e795c8d5bb
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-01 05:13:11 +0800

    Update functions.php

--- fourpaws-child/functions.php
+++ fourpaws-child/functions.php
@@ -227,3 +227,19 @@ function action_admin_head()
 }
 
 add_action('admin_head', 'action_admin_head');
+
+
+add_filter('get_the_archive_title', function ($title) {
+    if (is_category()) {
+        $title = single_cat_title('', false);
+    } elseif (is_tag()) {
+        $title = single_tag_title('', false);
+    } elseif (is_author()) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif (is_tax()) { //for custom post types
+        $title = sprintf(__('%1$s'), single_term_title('', false));
+    } elseif (is_post_type_archive()) {
+        $title = post_type_archive_title('', false);
+    }
+    return $title;
+});
