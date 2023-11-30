e40471b9181832c5327c11998194f1282e47d2b0
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-01 04:21:42 +0800

    Update functions.php

--- fourpaws-child/functions.php
+++ fourpaws-child/functions.php
@@ -245,9 +245,9 @@ function action_save_posts($post_id, $post, $update)
 	//Check your nonce!
 
 	//If calling wp_update_post, unhook this function so it doesn't loop infinitely
-	remove_action('save_post', 'action_save_posts');
 	// check the slug and run an update if necessary 
 	if ($post->post_title != $update->post_title) {
+		remove_action('save_post', 'action_save_posts');
 		$new_slug = sanitize_title($update->post_title);
 		wp_update_post(
 			array(
