c1c7c9b78c79cec77a344c745cb38bdce59ed43f
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 03:02:17 +0800

    Update courses.php

--- fourpaws-child/includes/courses.php
+++ fourpaws-child/includes/courses.php
@@ -687,18 +687,8 @@ function single_instructor_courses()
                     <article class="eltdf-cl-item eltdf-item-space post-3786 course type-course status-publish has-post-thumbnail hentry course-category-canine-qualifications course-tag-courses-home" data-name="ipet-network-level-2-award-in-responsible-dog-ownership" data-date="1701129600">
                         <div class="eltdf-cl-item-inner">
                             <div class="eltdf-cli-image">
-                                <img width="2048" height="1365" src="https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366.jpg" class="attachment-full size-full wp-post-image" alt="" decoding="async" srcset="https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366.jpg 2048w, https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366-300x200.jpg 300w, https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366-1024x683.jpg 1024w, https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366-768x512.jpg 768w, https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366-1536x1024.jpg 1536w, https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366-600x400.jpg 600w" sizes="(max-width: 2048px) 100vw, 2048px">
-                                <div class="eltdf-cli-category-holder">
-                                    <div class="level-holder">
-                                        <span class="icon">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="15.008" height="20.034" viewBox="0 0 15.008 20.034">
-                                                <path id="award" d="M14.2,15.344a5.5,5.5,0,0,0,1.977-.884l1.147,4.507a1.675,1.675,0,0,1-2.392,1.859L13,19.9a2.32,2.32,0,0,0-2,0l-1.933.928a1.675,1.675,0,0,1-2.392-1.859L7.82,14.46a5.5,5.5,0,0,0,1.977.884m4.4,0a9.95,9.95,0,0,1-4.4,0m4.4,0a5.342,5.342,0,0,0,4.043-3.915,9.052,9.052,0,0,0,0-4.266A5.342,5.342,0,0,0,14.2,3.247a9.95,9.95,0,0,0-4.4,0A5.342,5.342,0,0,0,5.755,7.162a9.055,9.055,0,0,0,0,4.266A5.342,5.342,0,0,0,9.8,15.344" transform="translate(-4.496 -1.996)" fill="none" stroke="#fff" stroke-width="2"></path>
-                                            </svg>
-                                        </span>
-                                        <span>Level 2</span>
-                                    </div>
-                                </div>
-
+                                <img width="2048" height="1365" src="<?= get_the_post_thumbnail_url(get_the_ID(), 'large') ?>" class="attachment-full size-full wp-post-image" alt="" decoding="async" srcset="<?= get_the_post_thumbnail_url(get_the_ID(), 'large') ?> 2048w, https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366-300x200.jpg 300w, https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366-1024x683.jpg 1024w, https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366-768x512.jpg 768w, https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366-1536x1024.jpg 1536w, https://fourpaws.theprogressteam.com/wp-content/uploads/2023/11/1D2A4366-600x400.jpg 600w" sizes="(max-width: 2048px) 100vw, 2048px">
+                                <?php do_action('course_level') ?>
                             </div>
                             <div class="eltdf-cli-text-holder">
                                 <div class="eltdf-cli-text-wrapper">
