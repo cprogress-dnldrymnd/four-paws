3a116dc3837c768f7e3d953e6e7959fd3cca989c
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 04:30:53 +0800

    Update courses.php

--- fourpaws-child/includes/courses.php
+++ fourpaws-child/includes/courses.php
@@ -685,14 +685,7 @@ function single_instructor_courses()
         'posts_per_page' => -1,
         'meta_key' => $meta_key,
         'meta_value' => 'yes',
-        'meta_compare' => 'IN',
-        'meta_query' => array(
-            array(
-                'key' => $meta_key,
-                'meta_value' => 'yes',
-                'compare' => '='
-            ),
-        ),
+        'meta_compare' => '=',
     );
     $query_course = new WP_Query($args);
 
