9eb277f6b322170d8dd12ba5989a7c4e4a8494a7
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 04:25:02 +0800

    Update courses.php

--- fourpaws-child/includes/courses.php
+++ fourpaws-child/includes/courses.php
@@ -675,9 +675,6 @@ add_action('location_map', 'location_map');
 //four-paws-lms\post-types\instructor\templates\single\parts\courses.php
 function single_instructor_courses()
 {
-    echo '<pre>';
-    var_dump(get_post_meta(3786));
-    echo '</pre>';
     $id = get_the_ID();
     $meta_key = '_location_' . $id;
     $args = array(
