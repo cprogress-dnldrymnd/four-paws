9775bcfc2847c313535c639594bd839a632ca449
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 01:14:25 +0800

    Update functions.php

--- fourpaws-child/functions.php
+++ fourpaws-child/functions.php
@@ -257,4 +257,4 @@ function action_excerpt_length($length)
 {
 	return 20;
 }
-add_filter('excerpt_length', 'action_excerpt_length', 999);
+add_filter('academist_elated_excerpt', 'action_excerpt_length', 999);
