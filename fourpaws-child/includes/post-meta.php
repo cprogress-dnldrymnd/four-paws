74c0d7dfb56385da187a7dff5b7c5be606b11349
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 01:28:53 +0800

    Update post-meta.php

--- fourpaws-child/includes/post-meta.php
+++ fourpaws-child/includes/post-meta.php
@@ -114,7 +114,7 @@ $locations = get_posts($args);
 
 $location_arr = array();
 foreach ($locations as $location) {
-	$location_arr[] = Field::make('checkbox', 'location_' . $location->post_id, __($location->post_title));
+	$location_arr[] = Field::make('checkbox', 'location_' . $location->ID, __($location->post_title));
 }
 Container::make('post_meta', __('Course Locations'))
 	->where('post_type', '=', 'course')
