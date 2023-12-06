a51660d61c11c0250faccfc4a5b653aca05293e6
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 12:34:29 +0800

    Update woocommerce.php

--- fourpaws-child/woocommerce.php
+++ fourpaws-child/woocommerce.php
@@ -5,23 +5,23 @@ Template Name: WooCommerce
 ?>
 <?php
 $eltdf_sidebar_layout  = academist_elated_sidebar_layout();
-$eltdf_grid_space_meta = academist_elated_options()->getOptionValue( 'woo_list_grid_space' );
-$eltdf_holder_classes  = ! empty( $eltdf_grid_space_meta ) ? 'eltdf-grid-' . $eltdf_grid_space_meta . '-gutter' : 'eltdf-grid-medium-gutter';
+$eltdf_grid_space_meta = academist_elated_options()->getOptionValue('woo_list_grid_space');
+$eltdf_holder_classes  = !empty($eltdf_grid_space_meta) ? 'eltdf-grid-' . $eltdf_grid_space_meta . '-gutter' : 'eltdf-grid-medium-gutter';
 
 get_header();
 academist_elated_get_title();
-get_template_part( 'slider' );
+get_template_part('slider');
 do_action('academist_elated_action_before_main_content');
 
 //Woocommerce content
-if ( ! is_singular( 'product' ) ) { ?>
+if (!is_singular('product')) { ?>
 	<div class="eltdf-container">
 		<div class="eltdf-container-inner clearfix">
-			<div class="eltdf-grid-row <?php echo esc_attr( $eltdf_holder_classes ); ?>">
+			<div class="eltdf-grid-row <?php echo esc_attr($eltdf_holder_classes); ?>">
 				<div <?php echo academist_elated_get_content_sidebar_class(); ?>>
 					<?php academist_elated_woocommerce_content(); ?>
 				</div>
-				<?php if ( $eltdf_sidebar_layout !== 'no-sidebar' ) { ?>
+				<?php if ($eltdf_sidebar_layout !== 'no-sidebar') { ?>
 					<div <?php echo academist_elated_get_sidebar_holder_class(); ?>>
 						<?php get_sidebar(); ?>
 					</div>
@@ -31,8 +31,8 @@ if ( ! is_singular( 'product' ) ) { ?>
 	</div>
 <?php } else { ?>
 	<div class="eltdf-container">
-		<?php do_action('product_archive_categories') ?>
 		<div class="eltdf-container-inner clearfix">
+			<?php do_action('product_archive_categories') ?>
 			<?php academist_elated_woocommerce_content(); ?>
 		</div>
 	</div>
