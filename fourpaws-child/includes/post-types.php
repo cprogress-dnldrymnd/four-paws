94f98626880a3ae3939842b0dbc0f99504f5e8f6
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-06 12:58:35 +0800

    Update post-types.php

--- fourpaws-child/includes/post-types.php
+++ fourpaws-child/includes/post-types.php
@@ -271,6 +271,32 @@ new newPostType(
 	)
 );
 
+
+new newPostType(
+	array(
+		'name'                => 'FAQs',
+		'singular_name'       => 'FAQ',
+		'icon'                => 'dashicons-question',
+		'exclude_from_search' => true,
+		'publicly_queryable'  => false,
+		'show_in_admin_bar'   => false,
+		'has_archive'         => false,
+		'supports'            => array('title', 'revisions', 'editor'),
+	)
+);
+new newTaxonomy(
+	array(
+		'taxonomy'  => 'location-category',
+		'post_type' => 'faqs',
+		'args'      => array(
+			'hierarchical' => true,
+			'label'        => 'Location Category',
+			'query_var'    => true,
+			'has_archive'  => true,
+			'show_in_rest' => true,
+		)
+	)
+);
 // Add the custom columns to the slider post type:
 add_filter('manage_slider_posts_columns', 'set_custom_edit_slider_columns');
 function set_custom_edit_slider_columns($columns)
