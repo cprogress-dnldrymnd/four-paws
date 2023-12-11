5a5b27930a0dcaf0d27b8562793e42e9ac55cd69
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-11 11:13:07 +0800

    Update functions.php

--- fourpaws-child/functions.php
+++ fourpaws-child/functions.php
@@ -296,7 +296,7 @@ function get_rc_shortcodes()
 		));
 
 		foreach ($posts as $post) {
-			$shortcode = '[rcblock id="' . $post->post_id . '"]';
+			$shortcode = '[rcblock id="' . $post . '"]';
 			if (strpos(get_the_content(), $shortcode) !== false) {
 				$rcblocks[] = $shortcode;
 			}
