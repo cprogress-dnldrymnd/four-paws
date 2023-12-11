74b4d0a775395bf9f437d17b4fa8caa7aac90521
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-11 13:54:29 +0800

    Update functions.php

--- fourpaws-child/functions.php
+++ fourpaws-child/functions.php
@@ -413,15 +413,18 @@ function rcblocks_admin()
 
 add_action('wp_head', 'rcblocks_admin');
 
-function action_post_updated($post_ID, $post_after, $post_before)
+function action_post_updated($post_ID)
 {
 	$post_type = get_post_type($post_ID);
 	if ($post_type == 'course') {
-		$reviews = get__post_meta_by_id($post_after->ID, 'course_reviews');
-		foreach ($reviews as $review) {
-			carbon_set_post_meta($review, 'course_' . $post_ID, true);
+		$testimonials = get__posts('testimonials');
+		foreach ($testimonials as $key => $testimonial) {
+			$field_id = get__post_meta_by_id($key, 'testimonial_' . $key);
+			if ($field_id) {
+				carbon_set_post_meta($key, 'course_' . $post_ID, true);
+			}
 		}
 	}
 }
 
-add_action('save_post', 'action_post_updated', 100, 3);
+add_action('save_post', 'action_post_updated', 100, 1);
