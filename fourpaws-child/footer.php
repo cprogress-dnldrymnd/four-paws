c13566af826f3cd8f2b5c813363b9fb3f653049c
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-11 12:01:12 +0800

    Update footer.php

--- fourpaws-child/footer.php
+++ fourpaws-child/footer.php
@@ -2,6 +2,16 @@
 <?php
 echo '<pre>';
 var_dump(get__post_meta('reviews'));
+
+carbon_set_post_meta(3874, 'reviews', array(
+    array(
+        'value' => 'post:testimonials:3628',
+        'type' => 'post',
+        'subtype' => 'testimonials',
+        'id' => '3628',
+    ),
+
+));
 echo '</pre>';
 do_action('rc_blocks_section');
 do_action('academist_elated_get_footer_template');
