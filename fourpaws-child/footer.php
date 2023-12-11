38c926c02c247320352e1e48339978788af80729
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-11 10:49:23 +0800

    Update footer.php

--- fourpaws-child/footer.php
+++ fourpaws-child/footer.php
@@ -5,6 +5,7 @@ if (is_post_type_archive('instructor')) {
 if (get_post_type() == 'instructor' && is_singular('instructor')) {
    get_rc_shortcodes_global('location_pages_bottom_content', true);
 }
+
+get_rc_shortcodes_global('footer_global_sections', true);
 ?>
-<?php rcblock_by_id("3366"); ?>
 <?php do_action('academist_elated_get_footer_template');
