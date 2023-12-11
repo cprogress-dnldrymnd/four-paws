6f429a2da55feb94209724368d2bfb535b5bf03c
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-11 09:59:25 +0800

    Update footer.php

--- fourpaws-child/footer.php
+++ fourpaws-child/footer.php
@@ -1,4 +1,16 @@
 <?php
+
+function get_shortcode_regex() {
+    global $shortcode_tags;
+    $tagnames = array_keys($shortcode_tags);
+    $tagregexp = join( '|', array_map('preg_quote', $tagnames) );
+
+    // WARNING! Do not change this regex without changing do_shortcode_tag() and strip_shortcodes()
+    return '(.?)\[('.$tagregexp.')\b(.*?)(?:(\/))?\](?:(.+?)\[\/\2\])?(.?)';
+}
+
+get_shortcode_regex();
+
 if (is_post_type_archive('instructor')) {
     rcblock_by_id("3462");
     rcblock_by_id("3507");
