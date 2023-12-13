cbf836e0aab85cc707249a5e2885faac32569acd
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-13 23:00:16 +0800

    Update functions.php

--- fourpaws-child/functions.php
+++ fourpaws-child/functions.php
@@ -460,17 +460,38 @@ function action_admin_enqueue_scripts($hook)
 	if ('edit.php' !== $hook) {
 		return;
 	}
+?>
+	<script>
+		jQuery(function($) {
 
+			const wp_inline_edit_function = inlineEditPost.edit;
 
-	wp_enqueue_script('wz-tutorials-bulk-edit', get_stylesheet_directory_uri() . '/admin/bulk-edit.js');
+			// we overwrite the it with our own
+			inlineEditPost.edit = function(post_id) {
 
-	wp_localize_script(
-		'wz-tutorials-bulk-edit',
-		'wz_tutorials_bulk_edit',
-		array(
-			'nonce' => wp_create_nonce('wz_tutorials_bulk_edit_nonce'),
-		)
-	);
+				// let's merge arguments of the original function
+				wp_inline_edit_function.apply(this, arguments);
+
+				// get the post ID from the argument
+				if (typeof(post_id) == 'object') { // if it is object, get the ID number
+					post_id = parseInt(this.getId(post_id));
+				}
+
+				// add rows to variables
+				const edit_row = $('#edit-' + post_id)
+				const post_row = $('#post-' + post_id)
+
+				const productPrice = $('.column-price', post_row).text().substring(1) //  remove $ sign
+				const featuredProduct = 'yes' == $('.column-featured', post_row).text() ? true : false;
+
+				// populate the inputs with column data
+				$(':input[name="price"]', edit_row).val(productPrice);
+				$(':input[name="featured"]', edit_row).prop('checked', featuredProduct);
+
+			}
+		});
+	</script>
+<?php
 }
 
 add_action('admin_enqueue_scripts', 'action_admin_enqueue_scripts');
