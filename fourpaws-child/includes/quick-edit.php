4bbae9edad700f535a0b1ed3797c1dd48387ea29
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-13 22:17:54 +0800

    Update quick-edit.php

--- fourpaws-child/includes/quick-edit.php
+++ fourpaws-child/includes/quick-edit.php
@@ -7,7 +7,7 @@ class Bulk_Edit
     {
         add_action('init', array($this, 'add_custom_columns'), 99);
         add_action('bulk_edit_custom_box', array($this, 'quick_edit_custom_box_function'));
-        add_action('quick_edit_custom_box', array($this, 'quick_edit_custom_box_function'));
+        add_action('quick_edit_custom_box', array($this, 'quick_edit_custom_box_function'), 10, 2);
     }
 
     function add_custom_columns()
@@ -71,7 +71,6 @@ class Bulk_Edit
                 }
         }
     }
-
 }
 
 
