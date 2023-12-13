56af20a6282e8e5813981cbd10ba676125548c3f
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-13 22:05:46 +0800

    Update quick-edit.php

--- fourpaws-child/includes/quick-edit.php
+++ fourpaws-child/includes/quick-edit.php
@@ -13,6 +13,33 @@ class Bulk_Edit
         add_filter('manage_' . $this->post_type . '_posts_columns', array($this, 'add_admin_columns'));
         add_action('manage_' . $this->post_type . '_posts_custom_column', array($this, 'populate_custom_columns'), 10, 2);
     }
+
+    function add_admin_columns()
+    {
+        $column_array['price'] = 'Price';
+        $column_array['featured'] = 'Featured product';
+        // the above code will add columns at the end of the array
+        // if you want columns to be added in another order, use array_slice()
+
+        return $column_array;
+    }
+
+    function populate_custom_columns($column_name, $post_id)
+    {
+
+        // if you have to populate more that one columns, use switch()
+        switch ($column_name) {
+            case 'price': {
+                    $price = get_post_meta($post_id, 'product_price', true);
+                    echo $price ? '$' . $price : '';
+                    break;
+                }
+            case 'featured': {
+                    echo get_post_meta($post_id, 'product_featured', true);
+                    break;
+                }
+        }
+    }
 }
 
 
