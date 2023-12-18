03edc08a1374193eb3a5ec5affe4961727b5bfec
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-18 22:05:00 +0800

    Update footer.php

--- fourpaws-child/footer.php
+++ fourpaws-child/footer.php
@@ -5,6 +5,7 @@ do_action('academist_elated_get_footer_template');
 <?php if (is_page(189) && isset($_GET['target'])) { ?>
     <script>
         jQuery(document).ready(function() {
+            console.log(window.location.hash);
             <?php if ($_GET['target'] == 'payment-plans') { ?>
                 jQuery('#ui-id-7').click();
             <?php } else if ($_GET['target'] == 'accommodation') { ?>
