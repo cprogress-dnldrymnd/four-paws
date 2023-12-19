1225257231608ac29efcf6ed7ecdd9a3d71a9658
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-19 14:18:41 +0800

    Update events-calendar.php

--- fourpaws-child/includes/events-calendar.php
+++ fourpaws-child/includes/events-calendar.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action('tribe_template_after_include:events/v2/components/events-bar/views', 'events_category', 10, 3);
+add_action('tribe_template_before_include:events/v2/components/events-bar/views', 'events_category', 10, 3);
 
 function events_category($file, $name, $template)
 {
