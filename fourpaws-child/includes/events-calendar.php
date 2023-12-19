29167f74d7b351a0f4df7e39208834e63aed6b9b
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-19 14:26:34 +0800

    Update events-calendar.php

--- fourpaws-child/includes/events-calendar.php
+++ fourpaws-child/includes/events-calendar.php
@@ -8,11 +8,21 @@ function events_category($file, $name, $template)
         'taxonomy'   => 'tribe_events_cat',
         'hide_empty' => false,
     ));
+
+    $selected = '';
+
 ?>
     <select name="events-category" id="events-category">
         <option value="">Category</option>
         <?php foreach ($terms as $term) { ?>
-            <option value="<?= get_term_link($term->term_id) ?>"><?= $term->name ?></option>
+            <?php
+
+            if (is_tax('tribe_events_cat', $term->term_id)) {
+                $selected = 'selected';
+            }
+            ?>
+
+            <option value="<?= get_term_link($term->term_id) ?>" <?= $selected ?>><?= $term->name ?></option>
         <?php } ?>
     </select>
 <?php
