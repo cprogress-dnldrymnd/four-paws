f6bed5ddaf5d349a4b4bd9b2f2d738d9bce3bc01
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-19 14:15:32 +0800

    Update events-calendar.php

--- fourpaws-child/includes/events-calendar.php
+++ fourpaws-child/includes/events-calendar.php
@@ -1,8 +1,5 @@
 <?php
-add_action( 'tribe_template_after_include:events/v2/month/mobile-events', function( $file, $name, $template ) {
-    echo '<button>My Button</button>';
-  }, 10, 3 );
 
-  add_action( 'tribe_template_before_include:events/v2/components/events-bar/views', function( $file, $name, $template ) {
+add_action('tribe_template_after_include:events/v2/components/events-bar/views', function ($file, $name, $template) {
     echo '<a href="#">My Link</a>';
-  }, 10, 3 );
+}, 10, 3);
