d479926b69c81997da3d786489deed1ee559cd8b
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2023-12-19 13:49:21 +0800

    Update header.php

--- fourpaws-child/header.php
+++ fourpaws-child/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
 	<?php
 	/**
@@ -9,10 +10,19 @@
 	 * @see academist_elated_user_scalable_meta - hooked with 10
 	 * @see academist_core_set_open_graph_meta - hooked with 10
 	 */
-	do_action( 'academist_elated_action_header_meta' );
-	
+	do_action('academist_elated_action_header_meta');
+
 	wp_head(); ?>
+
+	<?php if (isset($_GET['post_type']) && isset($_GET['s'])) { ?>
+		<style>
+			.eltdf-search-page-holder .eltdf-search-page-form {
+				display: none !important;
+			}
+		</style>
+	<?php } ?>
 </head>
+
 <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 	<?php
 	/**
@@ -21,24 +31,24 @@
 	 * @see academist_elated_get_side_area() - hooked with 10
 	 * @see academist_elated_smooth_page_transitions() - hooked with 10
 	 */
-	do_action( 'academist_elated_action_after_body_tag' ); ?>
+	do_action('academist_elated_action_after_body_tag'); ?>
 
-    <div class="eltdf-wrapper">
-        <div class="eltdf-wrapper-inner">
-					<div class="top-bar-bg">
-						
-					</div>
-            <?php
-            /**
-             * academist_elated_action_after_wrapper_inner hook
-             *
-             * @see academist_elated_get_header() - hooked with 10
-             * @see academist_elated_get_mobile_header() - hooked with 20
-             * @see academist_elated_back_to_top_button() - hooked with 30
-             * @see academist_elated_get_header_minimal_full_screen_menu() - hooked with 40
-             * @see academist_elated_get_header_bottom_navigation() - hooked with 40
-             */
-            do_action( 'academist_elated_action_after_wrapper_inner' ); ?>
-	        
-            <div class="eltdf-content" <?php academist_elated_content_elem_style_attr(); ?>>
-                <div class="eltdf-content-inner">
+	<div class="eltdf-wrapper">
+		<div class="eltdf-wrapper-inner">
+			<div class="top-bar-bg">
+
+			</div>
+			<?php
+			/**
+			 * academist_elated_action_after_wrapper_inner hook
+			 *
+			 * @see academist_elated_get_header() - hooked with 10
+			 * @see academist_elated_get_mobile_header() - hooked with 20
+			 * @see academist_elated_back_to_top_button() - hooked with 30
+			 * @see academist_elated_get_header_minimal_full_screen_menu() - hooked with 40
+			 * @see academist_elated_get_header_bottom_navigation() - hooked with 40
+			 */
+			do_action('academist_elated_action_after_wrapper_inner'); ?>
+
+			<div class="eltdf-content" <?php academist_elated_content_elem_style_attr(); ?>>
+				<div class="eltdf-content-inner">
