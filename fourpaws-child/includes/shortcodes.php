7c34f8583d599679f8345043a3637bc4116aaea1
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-01-02 23:18:16 +0800

    Update shortcodes.php

--- fourpaws-child/includes/shortcodes.php
+++ fourpaws-child/includes/shortcodes.php
@@ -392,3 +392,14 @@ function related_course()
 
 
 add_shortcode('related_course', 'related_course');
+
+
+
+function location_email_address()
+{
+    $location_email_address = get__post_meta('location_email_address');
+    return wp_kses($location_email_address, wpcf7dtx_get_allowed_field_properties('option'));
+}
+
+
+add_shortcode('location_email_address', 'location_email_address');
