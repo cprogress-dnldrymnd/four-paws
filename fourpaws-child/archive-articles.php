4dd12ee57332500bd9192eebf9f5a0c90a5cfdf1
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-16 00:13:33 +0800

    cs

--- fourpaws-child/archive-articles.php
+++ fourpaws-child/archive-articles.php
@@ -14,7 +14,7 @@ do_action('academist_elated_action_before_main_content');
     <div class="<?php echo esc_attr($eltdf_holder_params['inner']); ?>">
         <div class="eltdf-grid-row <?php echo esc_attr($holder_classes); ?>">
             <div <?php echo academist_elated_get_content_sidebar_class(); ?>>
-                <?php academist_elated_get_blog_type($blog_type); ?>
+                <?php academist_elated_get_blog_type(''); ?>
             </div>
 
             <div <?php echo academist_elated_get_sidebar_holder_class(); ?>>
