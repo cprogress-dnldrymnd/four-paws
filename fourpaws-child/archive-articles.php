630022655fe2561c7e5ee71a3c40d1c002aa5b39
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-16 00:12:57 +0800

    Create archive-articles.php

---
+++ fourpaws-child/archive-articles.php
@@ -0,0 +1,30 @@
+<?php
+$eltdf_blog_type = academist_elated_get_archive_blog_list_layout();
+academist_elated_include_blog_helper_functions('lists', $eltdf_blog_type);
+$eltdf_holder_params = academist_elated_get_holder_params_blog();
+
+get_header();
+academist_elated_get_title();
+do_action('academist_elated_action_before_main_content');
+?>
+
+<div class="<?php echo esc_attr($eltdf_holder_params['holder']); ?>">
+    <?php do_action('academist_elated_action_after_container_open'); ?>
+
+    <div class="<?php echo esc_attr($eltdf_holder_params['inner']); ?>">
+        <div class="eltdf-grid-row <?php echo esc_attr($holder_classes); ?>">
+            <div <?php echo academist_elated_get_content_sidebar_class(); ?>>
+                <?php academist_elated_get_blog_type($blog_type); ?>
+            </div>
+
+            <div <?php echo academist_elated_get_sidebar_holder_class(); ?>>
+                <?php get_sidebar('blog'); ?>
+            </div>
+        </div>
+    </div>
+
+    <?php do_action('academist_elated_action_before_container_close'); ?>
+</div>
+
+<?php do_action('academist_elated_action_blog_list_additional_tags'); ?>
+<?php get_footer(); ?>
