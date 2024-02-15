2666bc51c4b28653a6d168c25e4e90f86773fd54
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-16 00:34:15 +0800

    cs

--- fourpaws-child/framework/modules/blog/templates/parts/post-info/share.php
+++ fourpaws-child/framework/modules/blog/templates/parts/post-info/share.php
@@ -1,5 +1,6 @@
 <?php
 $share_type = isset($share_type) ? $share_type : 'list';
+echo $share_type;
 ?>
 <?php if ( academist_elated_core_plugin_installed() && academist_elated_options()->getOptionValue( 'enable_social_share' ) === 'yes' && academist_elated_options()->getOptionValue( 'enable_social_share_on_post' ) === 'yes' ) { ?>
     <div class="eltdf-blog-share">
--- fourpaws-child/single-articles.php
+++ fourpaws-child/single-articles.php
@@ -39,6 +39,8 @@ if (have_posts()) : while (have_posts()) : the_post();
 					<li class="eltdf-linkedin-share"> <a itemprop="url" class="eltdf-share-link" href="#" onclick="popUp=window.open('https://linkedin.com/shareArticle?mini=true&amp;url=https%3A%2F%2Fwww.fourpawsgroomschool.co.uk%2Fsarah-mackay-director-of-four-paws-groom-school-shines-on-channel-5s-dog-behaving-very-badly%2F&amp;title=Sarah+Mackay%2C+Director+of+Four+Paws+Groom+School%2C+Shines+on+Channel+5%26%238217%3Bs+%26%238220%3BDog+Behaving+Very+Badly%26%238221%3B', 'popupwindow', 'scrollbars=yes,width=800,height=400');popUp.focus();return false;"> <span class="eltdf-social-network-icon social_linkedin"></span> </a> </li>
 					<li class="eltdf-twitter-share"> <a itemprop="url" class="eltdf-share-link" href="#" onclick="window.open('https://twitter.com/intent/tweet?text=Introduction%3A+In+a+recent+episode+of+Channel+5%26%238217%3Bs+%26%238220%3BDog+Behaving+Very+Badly%2C%26%238221%3B+%28aired+on+Monday+8th+ https://www.fourpawsgroomschool.co.uk/sarah-mackay-director-of-four-paws-groom-school-shines-on-channel-5s-dog-behaving-very-badly/', 'popupwindow', 'scrollbars=yes,width=800,height=400');"> <span class="eltdf-social-network-icon social_twitter"></span> </a> </li>
 				</ul>
+
+				<?php echo academist_elated_get_social_share_html(array('type' => $share_type)); ?>
 			</div>
 
 			<?php do_action('academist_elated_action_before_container_close'); ?>
