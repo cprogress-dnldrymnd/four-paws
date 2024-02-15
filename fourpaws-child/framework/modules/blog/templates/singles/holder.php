dbf6763c5eaaa464de1a18e1d341bec805392651
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-16 00:07:51 +0800

    css

--- fourpaws-child/framework/modules/blog/templates/singles/holder.php
+++ fourpaws-child/framework/modules/blog/templates/singles/holder.php
@@ -2,7 +2,7 @@
 	<div <?php echo academist_elated_get_content_sidebar_class(); ?>>
 		<div class="eltdf-blog-holder eltdf-blog-single <?php echo esc_attr($blog_single_classes); ?>">
 			<?php do_action('single_featured_image'); ?>
-			<?= $blog_single_type ?>
+	
 			<?php academist_elated_get_blog_single_type($blog_single_type); ?>
 		</div>
 	</div>
