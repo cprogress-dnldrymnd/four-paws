a2935615534b335b98159384ef46bf31eef10094
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2024-02-16 00:28:59 +0800

    Update single-articles.php

--- fourpaws-child/single-articles.php
+++ fourpaws-child/single-articles.php
@@ -25,6 +25,29 @@ if (have_posts()) : while (have_posts()) : the_post();
 							<?php do_action('single_featured_image'); ?>
 
 							<?php academist_elated_get_blog_single_type('standard'); ?>
+							<div class="eltdf-post-info-bottom eltdf-post-info-bottom-real clearfix">
+								<div class="eltdf-post-info-bottom-left">
+								</div>
+								<div class="eltdf-post-info-bottom-right">
+									<div class="eltdf-blog-share">
+										<span class="eltdf-post-info-bottom-text">Share: </span>
+										<div class="eltdf-social-share-holder eltdf-list">
+											<ul>
+												<li class="eltdf-linkedin-share">
+													<a itemprop="url" class="eltdf-share-link" href="#" onclick="popUp=window.open('https://linkedin.com/shareArticle?mini=true&amp;url=https%3A%2F%2Fwww.fourpawsgroomschool.co.uk%2Fsarah-mackay-director-of-four-paws-groom-school-shines-on-channel-5s-dog-behaving-very-badly%2F&amp;title=Sarah+Mackay%2C+Director+of+Four+Paws+Groom+School%2C+Shines+on+Channel+5%26%238217%3Bs+%26%238220%3BDog+Behaving+Very+Badly%26%238221%3B', 'popupwindow', 'scrollbars=yes,width=800,height=400');popUp.focus();return false;">
+														<span class="eltdf-social-network-icon social_linkedin"></span>
+													</a>
+												</li>
+												<li class="eltdf-twitter-share">
+													<a itemprop="url" class="eltdf-share-link" href="#" onclick="window.open('https://twitter.com/intent/tweet?text=Introduction%3A+In+a+recent+episode+of+Channel+5%26%238217%3Bs+%26%238220%3BDog+Behaving+Very+Badly%2C%26%238221%3B+%28aired+on+Monday+8th+ https://www.fourpawsgroomschool.co.uk/sarah-mackay-director-of-four-paws-groom-school-shines-on-channel-5s-dog-behaving-very-badly/', 'popupwindow', 'scrollbars=yes,width=800,height=400');">
+														<span class="eltdf-social-network-icon social_twitter"></span>
+													</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
 
 						</div>
 					</div>
