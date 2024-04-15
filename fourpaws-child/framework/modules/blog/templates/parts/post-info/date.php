8521d18c72217aee7fa7162f20b7aaecb1e4c0ad
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-15 22:25:53 +0800

    Update date.php

--- fourpaws-child/framework/modules/blog/templates/parts/post-info/date.php
+++ fourpaws-child/framework/modules/blog/templates/parts/post-info/date.php
@@ -3,6 +3,7 @@ $month = get_the_time('m');
 $year = get_the_time('Y');
 $title = get_the_title();
 if (get_post_type() == 'post') {
+
 ?>
     <div itemprop="dateCreated" class="eltdf-post-info-date entry-date published updated">
         <?php if (empty($title) && academist_elated_blog_item_has_link()) { ?>
@@ -16,4 +17,16 @@ if (get_post_type() == 'post') {
                 <meta itemprop="interactionCount" content="UserComments: <?php echo get_comments_number(academist_elated_get_page_id()); ?>" />
     </div>
 
+<?php } else { ?>
+    <div itemprop="dateCreated" class="eltdf-post-info-date entry-date published updated">
+        <?php if (empty($title) && academist_elated_blog_item_has_link()) { ?>
+            <span itemprop="url">
+            <?php } else { ?>
+                <span itemprop="url">
+                <?php } ?>
+
+                <?php the_time(get_option('date_format')); ?>
+                </span>
+                <meta itemprop="interactionCount" content="UserComments: <?php echo get_comments_number(academist_elated_get_page_id()); ?>" />
+    </div>
 <?php } ?>
