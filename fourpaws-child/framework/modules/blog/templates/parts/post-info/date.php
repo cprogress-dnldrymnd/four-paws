232941af3a3f5aa1009f24819c8be6d6968d3693
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-15 22:24:57 +0800

    cs

--- fourpaws-child/framework/modules/blog/templates/parts/post-info/date.php
+++ fourpaws-child/framework/modules/blog/templates/parts/post-info/date.php
@@ -2,16 +2,18 @@
 $month = get_the_time('m');
 $year = get_the_time('Y');
 $title = get_the_title();
+if (get_post_type() == 'post') {
 ?>
-etst
-<div itemprop="dateCreated" class="eltdf-post-info-date entry-date published updated">
-    <?php if(empty($title) && academist_elated_blog_item_has_link()) { ?>
-        <a itemprop="url" href="<?php the_permalink() ?>">
-    <?php } else { ?>
-        <a itemprop="url" href="<?php echo get_month_link($year, $month); ?>">
-    <?php } ?>
+    <div itemprop="dateCreated" class="eltdf-post-info-date entry-date published updated">
+        <?php if (empty($title) && academist_elated_blog_item_has_link()) { ?>
+            <a itemprop="url" href="<?php the_permalink() ?>">
+            <?php } else { ?>
+                <a itemprop="url" href="<?php echo get_month_link($year, $month); ?>">
+                <?php } ?>
 
-        <?php the_time(get_option('date_format')); ?>
-        </a>
-    <meta itemprop="interactionCount" content="UserComments: <?php echo get_comments_number(academist_elated_get_page_id()); ?>"/>
-</div>
+                <?php the_time(get_option('date_format')); ?>
+                </a>
+                <meta itemprop="interactionCount" content="UserComments: <?php echo get_comments_number(academist_elated_get_page_id()); ?>" />
+    </div>
+
+<?php } ?>
