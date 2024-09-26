54cfc29c9aa3a8094f297238a525f23bb4db4cad
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2024-09-26 19:54:15 +0800

    Update header.php

--- fourpaws-child/header.php
+++ fourpaws-child/header.php
@@ -21,6 +21,18 @@
 			}
 		</style>
 	<?php } ?>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16689719883"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'AW-16689719883');
+	</script>
 </head>
 
 <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
