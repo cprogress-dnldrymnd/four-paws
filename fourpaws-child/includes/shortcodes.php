3146def458cb196acc47fe539baceede4df08b7a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-03-08 16:34:42 +0800

    Update shortcodes.php

--- fourpaws-child/includes/shortcodes.php
+++ fourpaws-child/includes/shortcodes.php
@@ -474,7 +474,7 @@ function course_category_filter()
 
 add_shortcode('course_category_filter', 'course_category_filter');
 
-
+/*
 add_filter('woocommerce_coupon_is_valid', 'bbloomer_coupon_valid_item_total_above', 9999, 3);
 function bbloomer_coupon_valid_item_total_above($valid, $coupon, $discount)
 {
@@ -511,7 +511,7 @@ function bbloomer_coupon_valid_item_total_above($valid, $coupon, $discount)
     } else {
         return true;
     }
-}
+}/*
 /*
 add_filter('woocommerce_coupon_is_valid', 'bbloomer_coupon_valid_item_total_above', 9999, 3);
 function bbloomer_coupon_valid_item_total_above($valid, $coupon, $discount)
